fffd25b3df280efb14cbd2429af6fc87a0ab52d8
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-02-07 17:45:13 +0300

    System.out.println

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class HelloWorld {
+     public static void main (String[] args){
+            System.out.println("Hello WORLD111 !");
+            System.out.println("Hello WORLD2 !");
+
+
+
+
+     }
+
+}
