5c2af3a3b4b20814e6ae036fd31ce97516ada2ee
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-02-07 17:46:55 +0300

    System out

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,12 @@ package com.company;
 
 public class HelloWorld {
      public static void main (String[] args){
-            System.out.println("Hello WORLD111 !");
+            System.out.println("Hello WORLD1 !");
             System.out.println("Hello WORLD2 !");
-
+            
+            System.out.println("sout - System.out.println'ın kısaltmasıdır ");
+            System.out.print("sou - ");  //Yan yana yazar.
+            System.out.printf("System.out.print'in kısaltmasıdır.");
 
 
 
