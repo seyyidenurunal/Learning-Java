ae4a8153c73aabfbdf63eb03a1b0f2ac9586c001
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-02-08 04:15:21 +0300

    Mantıksal Operatörler

---
+++ MantıksalOperatorler.java
@@ -0,0 +1,40 @@
+package Operatorler;
+
+public class MantıksalOperatorler {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+
+        System.out.println("a = b -- " + (a == b));
+        System.out.println("a < b -- " + (a < b));
+        System.out.println("a > b -- " + (a > b));
+        System.out.println("a >= b -- " + (a >= b));
+        System.out.println("a <= b -- " + (a <= b));
+        System.out.println("a != b -- " + (a != b));
+
+
+        // || veya
+        // && ve
+        
+
+        //True and True = True
+        //True and False = False
+        //False and False = False
+        //False and False = False
+
+        //True or True = True
+        //True or False = True
+        //False or False = True
+        //False or False = True
+
+
+
+
+
+
+
+
+    }
+}
