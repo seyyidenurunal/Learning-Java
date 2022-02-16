ded2714c97191dfe1d8c77fe06c79535378e3208
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-02-16 17:15:22 +0300

    OOP

---
+++ ClassStudent.java
@@ -0,0 +1,58 @@
+package OOP;
+
+public class ClassStudent {
+
+    private int no;
+    private String isim;
+    private int sınıf;
+    private boolean aktif;
+
+    public void setNo(int yeniNo){
+        this.no = yeniNo;
+    }
+
+    public void setSınıf(int yeniSınıf){
+        this.sınıf = yeniSınıf;
+    }
+
+
+
+    public ClassStudent(){
+
+    }
+
+    public ClassStudent(int no){
+        this.no = no;
+    }
+
+    public ClassStudent(int no, String isim){
+        this(no);
+        this.isim = isim;
+    }
+
+    public ClassStudent(int no, String isim, int sınıf){
+        this(no,isim);
+        this.sınıf= sınıf;
+    }
+
+    public ClassStudent(int no, String isim, int sınıf, boolean aktif){
+        //this.isim = isim;
+        //this.no = no;
+        //this.sınıf = sınıf;  //Buradaki kodları referansla kısaltıyoruz.
+        this(no,isim,sınıf); //Ve tek satır haline getiriyoruz. Buradaki constructur anlamına geliyor.
+        this.aktif = aktif;
+    }
+
+    public void ogrenciBilgileri(){
+       /*
+       if (aktif) {
+           System.out.println("Ad: " + isim + " -- Numara: " + no + "  --  Aktif : " + aktif + "  -- Sınıf : " + sınıf);
+       }else{
+           System.out.println("Öğrenci aktif değil....");
+       }
+       */
+        System.out.println("Ad: " + isim + " -- Numara: " + no + "  -- Sınıf : " + sınıf  + "  --  Aktif : " + aktif);
+
+    }
+
+}
---
+++ StudentClass.java
@@ -0,0 +1,38 @@
+package OOP;
+
+public class StudentClass {
+
+    public static void main(String[] args) {
+
+        ClassStudent[] tumOgrenciler = new ClassStudent[150];
+
+        ClassStudent c = new ClassStudent(458,"Cemile", 7,true);
+        c.ogrenciBilgileri();
+
+        ClassStudent ogr1 = new ClassStudent(154);
+        ClassStudent ogr2 = new ClassStudent(155, "Hayriye");
+        ClassStudent ogr3 = new ClassStudent(156, "Nuriye", 8);
+        ClassStudent ogr4 = new ClassStudent(157, "Nesime", 9,true );
+
+
+
+        ogr1.ogrenciBilgileri();
+        ogr2.ogrenciBilgileri();
+        ogr3.ogrenciBilgileri();
+        ogr4.ogrenciBilgileri();
+
+
+        tumOgrenciler[0] = ogr1;
+        tumOgrenciler[1] = ogr2;
+        tumOgrenciler[2] = ogr3;
+        tumOgrenciler[3] = ogr4;
+        tumOgrenciler[4] = new ClassStudent(15,"Ceylan"); //İstersek dizi içerisinde yeni bir nesne de oluşturabiliriz.
+
+        System.out.println("Listedeki Öğrenciler");
+        tumOgrenciler[0].ogrenciBilgileri();
+
+
+
+
+    }
+}
